00f0d3a923707149c5fe79d6c1c024b858319b81
samrat kumar <sam9999950@gmail.com>
2023-08-03 10:15:24 +0530

    completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser');
 const facilityRoutes = require('./routes/facilityRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
